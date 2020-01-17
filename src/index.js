624b8ea6204cfec0bd1a3545a204eaf3bb2e4c40
Jose <jozylopez27@gmail.com>
2020-01-17 15:32:24 -0600

    Api call/ Api module connected

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import $ from 'jquery';
 import './index.css';
 import api from './api';
-
+import bookmarklist from './bookmark-list';
 
 function main(){
     api.getBookmarks()
         .then(response => response.json())
-        .then(bookmarks => console.log('bookmarks'))
+        .then(bookmarks => console.log(bookmarks))
 }
 
 
