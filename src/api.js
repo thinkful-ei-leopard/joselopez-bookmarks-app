9de33a1008d1376aed9d1d263ae76a7f7003ecc8
Jose <jozylopez27@gmail.com>
2020-01-17 22:14:09 -0600

    able to create bookmark

--- src/api.js
+++ src/api.js
@@ -1,9 +1,48 @@
-export default {
-    getBookmarks
-};
 
 const BASE_URL = 'https://thinkful-list-api.herokuapp.com/[joselopez]';
 
-function getBookmarks() {
-    return fetch(`${BASE_URL}/bookmarks`)
+
+function listApiFetch(...args) {
+    let error;
+    return fetch(...args)
+        .then(response => {
+            if (!response.ok) {
+                error = {code: response.status};
+
+                if(!response.headers.get('content-type').includes('json')) {
+                    error.message = response.statusText;
+                    return Promise.reject(error)
+                }
+            }
+            return response.json();
+        })
+        .then(data => {
+            if(error) {
+                error.message = data.message;
+                return Promise.reject(error);
+            }
+            return data
+        });
+        
+};
+
+
+function getBookMarks() {
+    return listApiFetch(`${BASE_URL}/bookmarks`)
 }
+
+
+function createBookMark(title, url, desc = 'No Description', rating = 1){
+    const newBookMark = JSON.stringify({ title, url, desc, rating });
+    return listApiFetch(`${BASE_URL}/bookmarks`, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: newBookMark
+    });
+}
+
+export default {
+    listApiFetch,
+    getBookMarks,
+    createBookMark,
+};
--- src/index.js
+++ src/index.js
@@ -7,9 +7,8 @@ import bookmarklist from './bookmark-list';
 
 
 function main(){
-    api.getBookmarks()
-        .then(response => response.json())
-        .then(bookmarks => console.log(bookmarks));
+    api.getBookMarks()
+        .then(bookmarks => console.log(bookmarks))
     
     bookmarklist.bindEventListeners()
     bookmarklist.render()
